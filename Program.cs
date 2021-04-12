b2a72cc72b31a8c289e123b585fb1a4d7fcd925c
Mikael <mikaelsdr@icloud.com>
2021-04-12 15:46:55 +0200

    Code cleanup
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -11,52 +11,21 @@ using Microsoft.Extensions.Logging;
 using System.Collections;
 namespace Event_Portal
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
 
       CreateHostBuilder(args).Build().Run();
 
-      // System.Diagnostics.Debug.WriteLine("Destination reached");
-
-
     }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    
-                    User newUser = new User("Bob", "Rock", "bob.rock@gmail.com", "123",
-                    1
-                    );
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
 
-                  Event newEvent = new Event(2, "Köpenhamn", "2021-04-20", "2021-04-30",
-                  "08:00", "23:00", 1);
-
-
-
-                  Console.WriteLine(newUser.firstName + " " +  newUser.lastName);
-                  Console.WriteLine(newEvent.location + " " + newEvent.startDate);
-
-                    
-                  Console.WriteLine("Destination Reached");
-
-                  newEvent.invitedList.Add(newUser);
-
-
-                  Console.WriteLine("Invited new user: " + newUser.firstName);
-
-
-                  Console.WriteLine("Event list: " + newEvent.invitedList[0]);
-
-                  Console.WriteLine("End of the code");
-
-
-                  
-
-                });
-    }
+            });
+  }
 }
