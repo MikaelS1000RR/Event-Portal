0762b1ac206cc2d20608803a2c519b6786f899e2
MikaelS1000RR <mikaelsdr@icloud.com>
2021-04-22 10:19:05 +0200

    Added Required in CreateUserDto and removed Key false in User
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -1,21 +1,23 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Event_Portal.Models;
 
 namespace Event_Portal.Dtos
 {
   public record CreateUserDto
   {
+
+    [Required]
     public string FirstName { get; init; }
 
+    [Required]
     public string LastName { get; init; }
 
+    [Required]
     public string Email { get; init; }
 
+    [Required]
     public string Password { get; init; }
-
-    public List<Event> CreatedEvents { get; init; } = new();
-
-    public List<Event> JoinedEvents { get; init; } = new();
 
                    
   }
