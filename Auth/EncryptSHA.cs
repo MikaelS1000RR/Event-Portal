ae3838b81bc566f1aa639b3ba2ba6bd425a22e00
MikaelS1000RR <mikaelsdr@icloud.com>
2021-04-23 14:29:17 +0200

    Removed old code

--- Auth/EncryptSHA.cs
+++ Auth/EncryptSHA.cs
@@ -9,24 +9,7 @@ using System.Security.Cryptography;
 
 namespace Event_Portal.Auth
 {
-  /*public class EncryptSHA 
-   {
-     public static string GetShaData(string data)
-     {
-       SHA1 sha = SHA1.Create();
-       Byte[] hashData = sha.ComputeHash(Encoding.Default.GetBytes(data));
-       StringBuilder returnValue = new StringBuilder();
-       int i;
-       for (i = 0; i < hashData.Length-1; i++)
-       {
-         returnValue.Append(hashData[i].ToString());
-
-       }
-       return returnValue.ToString();
-     } 
-   }
- } */
-
+  
   public class PasswordHash
   {
     public const int SaltByteSize = 24;
