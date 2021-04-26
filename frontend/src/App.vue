ccc63a2fc3ccccea1180c1a0f0981e89b240ebc6
MikaelS1000RR <mikaelsdr@icloud.com>
2021-04-26 15:04:48 +0200

    CSS push
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
+    <Navbar />
     <v-main>
-     <Home/>
+      <Home />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-import Home from "./views/Home.vue"
+import Home from "./views/Home.vue";
+import Navbar from "./components/Navbar.vue";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-   Home
+    Home,
+    Navbar,
   },
 
   data: () => ({
@@ -24,28 +26,22 @@ export default {
 
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
+*,
+::before,
+::after {
+  padding: 0;
+  margin: 0;
 
-  *, ::before, ::after {
-    padding: 0;
-    margin: 0; 
-
-    background-image:
-    radial-gradient(at top left, black, transparent),
+  background-image: radial-gradient(at top left, black, transparent),
     radial-gradient(at top right, rgb(70, 0, 128), transparent),
-    radial-gradient(at bottom left,#6133ed68, transparent); 
-  }
-    
+    radial-gradient(at bottom left, #6133ed68, transparent);
+}
 
 body {
   margin: 0;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   overflow-x: hidden;
 }
-
-
-
-  
-
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+
+<div class="navbar-wrapper">
+
+    <v-card
+    flat
+    tile
+
+    class="v-card"
+  >
+    <v-toolbar
+      color="cyan"
+      dark
+    >
+      <v-app-bar-nav-icon class="hamburger"></v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      
+    </v-toolbar>
+    </v-card>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.navbar-wrapped {
+
+  
+
+}
+
+.hamburger {
+  align-self: flex-end;
+  
+}
+
+
+.v-card {
+
+ position: flex;
+
+}
+
+
+</style>
