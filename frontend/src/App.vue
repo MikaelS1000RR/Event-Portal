9d4396896826db903a26ac1fddc6d4f1c0b57475
MikaelS1000RR <mikaelsdr@icloud.com>
2021-04-26 14:45:02 +0200

    Changed background and imported google fonts
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,3 +21,31 @@ export default {
   }),
 };
 </script>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
+
+  *, ::before, ::after {
+    padding: 0;
+    margin: 0; 
+
+    background-image:
+    radial-gradient(at top left, black, transparent),
+    radial-gradient(at top right, rgb(70, 0, 128), transparent),
+    radial-gradient(at bottom left,#6133ed68, transparent); 
+  }
+    
+
+body {
+  margin: 0;
+  font-family: 'Montserrat', sans-serif;
+  overflow-x: hidden;
+}
+
+
+
+  
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="home">
-    <div class="numberCounter">
-      <p>{{$store.getters.getAllEvents}}</p>
-      
-      <button v-on:click="getEvents">Fetch all events</button>
-      
-      
-    </div>
-
-    <div class="specEvent">
-      <p>{{$store.state.specEvent.location}}</p>
-      <button v-on:click="getSpecEvent">Fetch specific event</button>
-
-
-      <p>{{$store.state.users}}</p>
-       <button v-on:click=" getUsers">Fetch users</button>
-      <p>{{$store.state.specUser}}</p>
-       <button v-on:click="getSpecUser">Fetch  specific user</button>
-    </div>
+    
   </div>
 </template>
 
@@ -52,32 +35,20 @@ export default {
     getSpecEvent(){
       return this.$store.dispatch('fetchSpecEvent')
     },
-    getUsers(){
-      return this.$store.dispatch('fetchUsers')
-    },
-    getSpecUser(){
-      return this.$store.dispatch('fetchSpecUser')
-    }
+    
   }
   
 }
 </script>
 
 <style scoped>
- .home{
-   width:100%;
-   height:100vh;
-   background-color:pink;
-   position: relative;
- }
 
- .numberCounter, .specEvent{
-    width: 640px; /* Can be in percentage also. */
-    height: 50%;
-    margin: 0 auto;
-    padding: 10px;
-    position: relative;
-   background-color: white;
-   
- }
+  .home {
+
+    width: 100%;
+    height: 100vh;
+
+
+  }
+ 
 </style>
