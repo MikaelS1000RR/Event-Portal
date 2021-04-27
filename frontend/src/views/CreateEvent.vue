9fc6211eeab379b3ac4d3312f9ab76a13fd8b0ee
MikaelS1000RR <mikaelsdr@icloud.com>
2021-04-27 14:34:25 +0200

    Created new file called Form.vue in components.
    Moved the code from CreateEvent to Form

--- frontend/src/views/CreateEvent.vue
+++ frontend/src/views/CreateEvent.vue
@@ -1,60 +1,12 @@
 <template>
   
   <div class="create-event-container">
-
-    <div class="wrapper">
-      <h3>Create new event</h3>
-      <form action="">
-        <p>
-          <label for="">Event name</label>
-          <input type="text">
-        </p>
-        <p>
-          <label for="">Location</label>
-          <input type="text">
-        </p>
-
-        <p>
-          <label for="">Time</label>
-          <input type="time">
-        </p>
-
-         <p>
-          <label for="">Date</label>
-          <input type="date">
-        </p>
-
-         <p>
-          <label for="">Description</label>
-           <textarea name="" id="" cols="30" rows="7"></textarea>
-        </p>
-
-       <p class="checkbox">
-       
-         
-        <label for="">Public</label>
-        <input type="checkbox" id="public">
-               
-        <label for="">Private</label>
-        <input type="checkbox" id="private">
-        
-       
-          </p>
-
-
-        <p class="input-file-wrapper">
-          <label for="upload">Upload your photo</label>
-          <input type="file" name="" id="upload">
-        </p>
-        
-        <p>
-          <button>Create</button>
-        </p>
-      </form>
+        <Form/>
+    
     </div>
 
     
-</div>
+
 
 
 
@@ -63,13 +15,17 @@
 
 
 <script>
+import Form from "../components/Form.vue";
 
 
 
 export default {
 
   name: "CreateEvent",
-  components: {},
+  components: {
+    Form
+    
+    },
 
   
 
@@ -82,87 +38,6 @@ export default {
 
 <style scoped>
 
-* {
-  box-sizing: border-box;
-}
-
-body {
-  padding-top: 1rem;
-}
-
-.wrapper {
-  max-width: 700px;
-  margin: 0 auto;
-  padding: 1em;
-  background: #f9f9f9;
-  border: 1px solid #c1c1c1;
-}
-
-h3 {
-  margin: 0;
-}
-
-input:focus,
-textarea:focus {
-  outline: 3px solid rgba(107, 21, 206, 0.233);
-}
-
-input,
-textarea,
-button {
-  width: 100%;
-  border: 1px solid #000;
-}
-
-.wrapper > * {
-  padding: 1em;
-}
-
-form label {
-  display: block;
-}
-
-form p {
-  margin: 0;
-}
-
-button,
-input,
-textarea {
-  padding: 1em;
-}
-
-button {
-  background: lightgrey;
-  width: 100%;
-  border: 0;
-}
-button:hover, button:focus {
-  background: rgba(107, 21, 206, 0.548);
-  outline: 0;
-}
-
-form {
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-gap: 20px;
-}
-form p {
-  grid-column: 1 / 2;
-}
-
-.input-file-wrapper {
-  grid-column: 2 / 3;
-  grid-row: 1 / 2;
-}
-
-
-.checkbox {
-  display: flex;
-  flex-direction: row;
-  justify-content: center; 
-  align-items: center;
-}
 
 
 </style>
