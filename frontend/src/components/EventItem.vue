0a36f5f0c2218bf139a7b22a05aab6bbc4cb5736
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-04-27 07:56:06 +0200

    Added eventItem for eventList

---
+++ frontend/src/components/EventItem.vue
@@ -0,0 +1,76 @@
+<template>
+  
+   <div class="event-card" 
+     @click="redirect">
+               <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+         <div class="access">
+       <p>Public</p>
+        </div>
+         <v-img 
+         class="event-img"
+          height="200px"
+          src="https://s24193.pcdn.co/wp-content/uploads/2017/06/Madilyn-Smith-Entity-Squidward1.png"
+        >
+          <v-card-title>
+            <p>{{ event.location }}</p>
+            </v-card-title>
+         </v-img>
+        </v-card>
+
+     </div>
+</template>
+
+<script>
+export default {
+props:['event'],
+
+  methods:{
+    redirect(){
+      this.$router.push('/details/' + this.event.id)
+    
+    }
+  }
+}
+</script>
+
+<style scoped >
+
+.event-card {
+   text-align: center;
+   padding:1vw;
+   height:75%;
+   display:flex;
+   align-items: center;
+    transition: transform .2s; 
+  
+}
+
+.event-card:hover{
+  cursor: pointer;
+  transform: scale(1.1);
+ 
+
+}
+
+.v-card__title{
+  justify-content: center;
+  font-weight: bold !important;
+  margin-top:4vw;
+  
+}
+
+.access{
+  background-color: white;
+  height:20px;
+
+  
+}
+
+.event-img{
+
+  text-align: center;
+
+  
+}
+
+</style>
--- frontend/src/components/EventList.vue
+++ frontend/src/components/EventList.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="wrapper">
    <div class="event-list">
-     <div class="event-card" 
-     v-for="myEvent of events" 
-     :key="myEvent.id"
-     @click="redirect">
-               <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-         <div class="access">
-       <p>Public</p>
-        </div>
-         <v-img 
-         class="event-img"
-          height="200px"
-          src="https://s24193.pcdn.co/wp-content/uploads/2017/06/Madilyn-Smith-Entity-Squidward1.png"
-        >
-          <v-card-title>
-            <p>{{ myEvent.location }}</p>
-            </v-card-title>
-         </v-img>
-        </v-card>
-
-     </div>
+     <EventItem v-for="event of events" :key="event.id" :event="event"/>
    </div>
   </div>
 </template>
 
 <script>
+import EventItem from './EventItem.vue'
 export default {
+  components:{
+   EventItem
+  },
   computed: {
     events() {
       return this.$store.state.events;
@@ -35,11 +20,7 @@ export default {
   created() {
     this.$store.dispatch("fetchEvents");
   },
-  methods:{
-    redirect(){
-      
-    }
-  }
+
 };
 </script>
 
@@ -65,41 +46,5 @@ export default {
   position: relative;
 }
 
-.event-card {
-   text-align: center;
-   padding:1vw;
-   height:75%;
-   display:flex;
-   align-items: center;
-    transition: transform .2s; 
-  
-}
 
-.event-card:hover{
-  cursor: pointer;
-  transform: scale(1.1);
- 
-
-}
-
-.v-card__title{
-  justify-content: center;
-  font-weight: bold !important;
-  margin-top:4vw;
-  
-}
-
-.access{
-  background-color: white;
-  height:20px;
-
-  
-}
-
-.event-img{
-
-  text-align: center;
-
-  
-}
 </style>
