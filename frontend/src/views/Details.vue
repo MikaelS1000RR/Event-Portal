6e8444843a65c97c42e450cfb64b3dfd953c5dab
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-04-28 08:41:44 +0200

    Made output for event time

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -1,273 +1,210 @@
 <template>
   <div class="details-wrapper">
     <vue-fontawesome icon="file" size="2" color="lightgray"></vue-fontawesome>
-    <v-card
- :loading="loading"
-    class="mx-auto my-12"
-  outlined
-  shaped
-  tile
->
+    <v-card :loading="loading" class="mx-auto my-12" outlined shaped tile>
+      <div class="event-name">
+        <p>{{ currEvent.name }}</p>
+      </div>
 
-<div class="event-name">
-<p>{{currEvent.name}}</p>
-</div>
+      <div class="event-details">
+        <div class="event-info">
+          <ul>
+            <li>
+              <v-icon large color="black">
+                mdi-domain
+              </v-icon>
+              <p>{{ currEvent.location }}</p>
+            </li>
 
-<div class="event-details">
+            <li>
+              <v-icon large color="black">
+                mdi-clock-time-nine-outline
+              </v-icon>
 
-<div class="event-info">
- <ul>
-   <li>
-      <v-icon
-      large
-      color="black"
-    >
-      mdi-domain
-    </v-icon>
-     <p>{{currEvent.location}}</p>
-   </li>
+              <p>{{ time }}</p>
+            </li>
 
-   <li>
-      <v-icon
-      large
-      color="black"
-    >
-      mdi-clock-time-nine-outline
-    </v-icon>
+            <li>
+              <v-icon large color="black">
+                mdi-calendar-range
+              </v-icon>
 
-    
-    <p>{{currEvent.endDateTime}}</p>
-     
-   </li>
+              <p>{{ dates }}</p>
+            </li>
 
-   <li>
-      <v-icon
-      large
-      color="black"
-    >
-      mdi-calendar-range
-    </v-icon>
+            <li>
+              <v-icon large color="black">
+                mdi-account-circle-outline
+              </v-icon>
+              <p>{{ hostUser.firstName }} {{ hostUser.lastName }}</p>
+            </li>
 
-    <p>{{dates}}</p>
-   </li>
+            <li>
+              <v-icon large color="black" v-if="currEvent.access === 'private'">
+                mdi-lock
+              </v-icon>
 
-   <li>
-      <v-icon
-      large
-      color="black"
-    >
-      mdi-account-circle-outline
-    </v-icon>
-   <p>{{hostUser.firstName}}  {{hostUser.lastName}}</p>
-  
-  
+              <v-icon large color="black" v-else>
+                mdi-lock-open
+              </v-icon>
 
-   </li>
+              <p>{{ currEvent.access }}</p>
+            </li>
+          </ul>
+        </div>
 
-   <li>
-      <v-icon
-      large
-      color="black"
-      v-if="currEvent.access === 'private'"
-    >
-      mdi-lock
-    </v-icon>
+        <div class="event-desc">
+          <div class="description">
+            <p>Description</p>
+          </div>
 
-    <v-icon
-      large
-      color="black"
-      v-else
-    >
-      mdi-lock-open
-    </v-icon>
-
-    <p>{{currEvent.access}}</p>
-   </li>
-
-   
-
- </ul>
-</div>
-
-<div class="event-desc">
-  <div class="description">
-     <p>Description</p>
-  </div>
- 
-  <p>{{currEvent.description}}</p>
- 
-</div>
-
-</div>
- <div class="btn-join">
- <v-btn
- elevation="11"
-  x-large
->Join</v-btn>
-</div>
-
-
-</v-card>
+          <p>{{ currEvent.description }}</p>
+        </div>
+      </div>
+      <div class="btn-join">
+        <v-btn elevation="11" x-large>Join</v-btn>
+      </div>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    getDate(eventDate) {
+      var startDt = new Date(eventDate);
+      const startDay = startDt.getDate(),
+        startMonth = startDt.getMonth(),
+        date = startDay + "/" + (startMonth + 1);
+      return date;
+    },
 
-  methods:{
-        getDate(eventDate){
-            var startDt=new Date(eventDate);
-                  const startDay = startDt.getDate(),
-             startMonth = startDt.getMonth(),
-              date=startDay +
-              "/" +
-              (startMonth + 1)
-              return date
+    getTime(eventDate){
+      var dt=new Date(eventDate);
 
-        }
+      const hours = dt.getHours(),
+      minutes = dt.getMinutes(),
+      time=hours + " : " + minutes;
+      return time;
+    }
   },
 
-   computed: {
+  computed: {
     currEvent() {
-      
-      return this.$store.state.specEvent
+      return this.$store.state.specEvent;
     },
-    hostUser(){
-  
-       return this.$store.state.specUser
+    hostUser() {
+      return this.$store.state.specUser;
     },
-     id() {
+    id() {
       return this.$route.params.id;
     },
 
-    dates(){
-           /* var dt = new Date(this.currEvent.startDateTime);
+    dates() {
+      const startDate = this.getDate(this.currEvent.startDateTime);
+      const endDate = this.getDate(this.currEvent.endDateTime);
 
-          var day = dt.getDate(),
-            month = dt.getMonth(),
-           
-            hours = dt.getHours(),
-            minutes = dt.getMinutes(),
-            seconds = dt.getSeconds();*/
+      if (startDate === endDate) {
+        return startDate;
+      }
+      if (startDate !== endDate) {
+        const generalDate = `${startDate} - ${endDate}`;
+        return generalDate;
+      }
 
-            const startDate=this.getDate(this.currEvent.startDateTime);
-            const endDate=this.getDate(this.currEvent.endDateTime);
-
-            if(startDate===endDate)
-            {
-              return startDate
-            }
-            if(startDate !== endDate)
-            {
-              const generalDate=`${startDate} - ${endDate}`
-              return generalDate
-            }
-
-            
-            
-            return this.getDate(this.currEvent.startDateTime);
-
-
-  
+      return this.getDate(this.currEvent.startDateTime);
     },
-   
+
+    time(){
+      const startTime=this.getTime(this.currEvent.startDateTime);
+      const endTime=this.getTime(this.currEvent.endDateTime);
+      const times=`${startTime} - ${endTime}`;
+      return times;
+
+    }
   },
- async created() {
-  
+  async created() {
     await this.$store.dispatch("fetchSpecEvent", this.id);
-
   },
-  
-
-  
-}
+};
 </script>
 
 <style scoped>
-
-.details-wrapper{
-  width:100%;
-   display: flex;
+.details-wrapper {
+  width: 100%;
+  display: flex;
   justify-content: center;
   align-items: center;
-
-
 }
 
-.my-12{
-  width:90vw;
-  
+.my-12 {
+  width: 90vw;
+
   background-color: white;
-     display: flex;
-     flex-direction: column;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: flex-end;
-  padding-right:2vw;
-  
+  padding-right: 2vw;
 }
 
-.event-name{
-  width:100%;
-  height:20%;
+.event-name {
+  width: 100%;
+  height: 20%;
 
   font-weight: bold;
-  font-size:1.5vw;
-  padding-left:2vw;
-  padding-top:1vw;
+  font-size: 1.5vw;
+  padding-left: 2vw;
+  padding-top: 1vw;
 }
 
-.event-details{
-
+.event-details {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   justify-content: center;
   align-items: center;
-  width:100%;
-
+  width: 100%;
 }
 
 ul {
-    list-style-type: none;
+  list-style-type: none;
 }
 
-ul>li{
+ul > li {
   display: flex;
   flex-direction: row;
-  padding:1.5vw;
+  padding: 1.5vw;
   align-items: center;
   text-align: center;
 }
 
-li>p{
-  margin-top:1.2vw;
-  padding-left:1vw;
-  font-size:1.2vw;
+li > p {
+  margin-top: 1.2vw;
+  padding-left: 1vw;
+  font-size: 1.2vw;
 }
 
-.event-desc{
-  display:grid;
+.event-desc {
+  display: grid;
   grid-auto-rows: 15% 85%;
- height:100%;
-padding-top:2.6vw;
+  height: 100%;
+  padding-top: 2.6vw;
 }
 
-.description{
+.description {
   font-weight: 500;
- 
 }
 
-.btn-join{
- margin-top:-3vw;
- padding-bottom:1vw;
-  margin-right:8vw;
+.btn-join {
+  margin-top: -3vw;
+  padding-bottom: 1vw;
+  margin-right: 8vw;
 }
-.theme--light.v-btn.v-btn--has-bg{
+.theme--light.v-btn.v-btn--has-bg {
   background-color: var(--buttonPurple);
 }
-.v-btn:not(.v-btn--round).v-size--x-large{
-  width:10vw;
-  height:5vh;
+.v-btn:not(.v-btn--round).v-size--x-large {
+  width: 10vw;
+  height: 5vh;
 }
-
-
 </style>
