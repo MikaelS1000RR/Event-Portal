e2c29138ad2c5db2674bc8f89140e4dcf35908d2
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-04-30 16:34:32 +0200

    Hot fix
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>

--- frontend/src/views/UpdateEvent.vue
+++ frontend/src/views/UpdateEvent.vue
@@ -1,209 +1,194 @@
 <template>
   <div class="update-wrapper">
-   
-     <form @submit.prevent="saveChanges">
-       <v-card :loading="loading" class="mx-auto my-12" outlined shaped tile>
-   
-    
-
+    <form @submit.prevent="saveChanges">
+      <v-card :loading="loading" class="mx-auto my-12" outlined shaped tile>
         <div class="event-details">
-        <div class="event-info">
-          <ul>
-            <li>
-               
+          <div class="event-info">
+            <ul>
+              <li>
+                <img
+                  class="detailedImg"
+                  src="https://www.flaticon.com/svg/vstatic/svg/1250/1250252.svg?token=exp=1619784941~hmac=be8b5c2880fd0496925c51adf3f5f626"
+                  alt=""
+                />
+                <input type="text" v-model="eventName" placeholder="" />
+              </li>
+              <li>
+                <img
+                  class="detailedImg"
+                  src="https://www.flaticon.com/svg/vstatic/svg/4585/4585617.svg?token=exp=1619724567~hmac=1a64feb6ca9b8ef9acc92eb3c6aeb715"
+                  alt=""
+                  srcset=""
+                />
+                <input type="text" placeholder="" v-model="location" />
+              </li>
 
-        <img class="detailedImg" src="https://www.flaticon.com/svg/vstatic/svg/1250/1250252.svg?token=exp=1619784941~hmac=be8b5c2880fd0496925c51adf3f5f626" alt="">
-          <input type="text" v-model="eventName" placeholder=""/>
-      
+              <li>
+                <img
+                  class="detailedImg"
+                  src="https://www.flaticon.com/svg/vstatic/svg/4585/4585147.svg?token=exp=1619724730~hmac=bab92bca9cca01f2c559c82059f750b5"
+                  alt=""
+                  srcset=""
+                />
 
-       
-            </li>
-            <li>
-              <img
-                class="detailedImg"
-                src="https://www.flaticon.com/svg/vstatic/svg/4585/4585617.svg?token=exp=1619724567~hmac=1a64feb6ca9b8ef9acc92eb3c6aeb715"
-                alt=""
-                srcset=""
-              />
-              <input type="text" placeholder="" v-model="location">
-            </li>
+                <input
+                  type="datetime-local"
+                  placeholder=""
+                  v-model="startTimeAndDate"
+                />
+                <span>to</span>
+                <input
+                  type="datetime-local"
+                  placeholder=""
+                  v-model="endTimeAndDate"
+                />
+              </li>
 
-            <li>
-              <img
-                class="detailedImg"
-                src="https://www.flaticon.com/svg/vstatic/svg/4585/4585147.svg?token=exp=1619724730~hmac=bab92bca9cca01f2c559c82059f750b5"
-                alt=""
-                srcset=""
-              />
+              <li>
+                <label for="public">Public</label>
+                <img class="door" src="../assets/door.png" />
+                <input
+                  id="public"
+                  value="Public"
+                  type="checkbox"
+                  v-model="publicAccess"
+                  @change="disablePrivate"
+                />
 
-            <input type="datetime-local" placeholder="" v-model="startTimeAndDate">
-            <span>to</span>
-            <input type="datetime-local" placeholder="" v-model="endTimeAndDate">
-            </li>
-
-
-            <li>
-             
-                 <label for="public">Public</label>
-        <img class="door" src="../assets/door.png">
-        <input id="public" value="Public" type="checkbox" v-model="publicAccess"  @change="disablePrivate">
-        
-            
-        <label for="private" class="lock-label">Private</label>
-        <img class="lock" src="../assets/lock.png">
-        <input  id="private" value="Private" type="checkbox" v-model="privateAccess" @change="disablePublic">
-            
-            </li>
-            <li>
- <div class="save-btn">
-          
-              
-    <v-btn
-      class="ma-2"
-      :loading="loading"
-      :disabled="loading"
-      color="success"
-      @click="loader = 'loading'"
-      type="submit"
-    >
-      Save changes
-      <template v-slot:loader>
-        <span>Saving...</span>
-      </template>
-    </v-btn>
-
+                <label for="private" class="lock-label">Private</label>
+                <img class="lock" src="../assets/lock.png" />
+                <input
+                  id="private"
+                  value="Private"
+                  type="checkbox"
+                  v-model="privateAccess"
+                  @change="disablePublic"
+                />
+              </li>
+              <li>
+                <div class="save-btn">
+                  <v-btn
+                    class="ma-2"
+                    :loading="loading"
+                    :disabled="loading"
+                    color="success"
+                    @click="loader = 'loading'"
+                    type="submit"
+                  >
+                    Save changes
+                    <template v-slot:loader>
+                      <span>Saving...</span>
+                    </template>
+                  </v-btn>
+                </div>
+              </li>
+            </ul>
           </div>
-            </li>
-          </ul>
-          </div>
-
-         
-
-
         </div>
-         
 
-           <div class="event-desc">
+        <div class="event-desc">
           <div class="description">
             <p>Description</p>
           </div>
 
-         <textarea v-model="description" name="" id="" cols="30" rows="7"> </textarea>
-
-
-
-          
+          <textarea v-model="description" name="" id="" cols="30" rows="7">
+          </textarea>
         </div>
-     
-    
-    </v-card>
-     </form>
-
-     <div class="btnDiv"></div>
-     </div>
-  
+      </v-card>
+    </form>
+  </div>
 </template>
 
 <script>
-
 export default {
-
-  data(){
-   return{
+  data() {
+    return {
       eventName: this.$store.state.specEvent.name,
-      location:  this.$store.state.specEvent.location,
-      startTimeAndDate: '',
-      endTimeAndDate: 'drgrdgdgr',
-      description: '',
-     publicAccess: true,
+      location: this.$store.state.specEvent.location,
+      startTimeAndDate: "",
+      endTimeAndDate: "drgrdgdgr",
+      description: "",
+      publicAccess: true,
       privateAccess: false,
       access: "",
-      hostId: '061eb70c-7055-4d07-a584-b3c20cd59d73',
+      hostId: "061eb70c-7055-4d07-a584-b3c20cd59d73",
       loader: null,
-         loading: false,
-   }
+      loading: false,
+    };
   },
-      watch: {
-      loader () {
-        const l = this.loader
-        this[l] = !this[l]
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
 
-        setTimeout(() => (this[l] = false), 3000)
+      setTimeout(() => (this[l] = false), 3000);
 
-        this.loader = null
-      },
+      this.loader = null;
     },
-   methods:{
-     saveChanges(){
-       this.loading=!false
-       setTimeout(() => {
-        this.loading!=true
-        
-       }, 2000)
+  },
+  methods: {
+    saveChanges() {
+      this.loading = !false;
+      setTimeout(() => {
+        this.loading != true;
+      }, 2000);
 
-       console.log('saving changes');
-        const startDateTime=this.$store.state.specEvent.startDateTime
-    const endDateTime=this.$store.state.specEvent.startDateTime
-
-     },
-       disablePublic(){
-     this.publicAccess=false;
-   },
-   disablePrivate(){
-     this.privateAccess=false;
-   },
-   },
+      console.log("saving changes");
+      const startDateTime = this.$store.state.specEvent.startDateTime;
+      const endDateTime = this.$store.state.specEvent.startDateTime;
+    },
+    disablePublic() {
+      this.publicAccess = false;
+    },
+    disablePrivate() {
+      this.privateAccess = false;
+    },
+  },
   computed: {
     currEvent() {
-      
       return this.$store.state.specEvent;
     },
-   
-  }
-
-}
+  },
+};
 </script>
 
 <style scoped>
-
-.update-wrapper{
-  width:100%;
-  height:100vh;
-  color:white;
- 
+.update-wrapper {
+  width: 100%;
+  height: 100vh;
+  color: white;
 }
 
 .detailedImg {
   width: 8%;
-  padding-right:1vh;
+  padding-right: 1vh;
 }
-
 
 .event-desc {
   display: grid;
-  width:50vw;
+  width: 50vw;
   grid-auto-rows: 15% 85%;
 
   padding-top: 2.6vw;
-  padding-bottom:10vh;
+  padding-bottom: 10vh;
 }
 
-.v-application .success{
+.v-application .success {
   background-color: var(--buttonPurple) !important;
 }
 
-
-
-li>span{
+li > span {
   padding: 0 1vw 0 1vw;
 }
 
-input, textarea {
+input,
+textarea {
   border: 1px solid #000 !important;
-  padding: .5vw 1vw .5vw 1vw;
+  padding: 0.5vw 1vw 0.5vw 1vw;
 }
 
-input:focus, textarea:focus {
+input:focus,
+textarea:focus {
   outline: 3px solid rgba(107, 21, 206, 0.233);
 }
 
@@ -215,10 +200,9 @@ input:focus, textarea:focus {
 
   background-color: white;
   display: flex;
- flex-direction: row;
+  flex-direction: row;
   padding-right: 2vw;
 }
-
 
 ul {
   list-style-type: none;
@@ -238,20 +222,14 @@ li > p {
   font-size: 1.2vw;
 }
 
-
 .door {
   width: 9%;
-  
-
 }
-.lock-label{
-  margin-left:2.5vw;
+.lock-label {
+  margin-left: 2.5vw;
 }
 
 .lock {
   width: 7%;
-
-
 }
-
 </style>
