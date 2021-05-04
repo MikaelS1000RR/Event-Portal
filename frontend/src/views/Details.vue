d539a20808353e4d5984a4e4c14900e7d2af439f
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-05-04 21:09:00 +0200

    Fixed timing on popups

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -1,76 +1,65 @@
 <template>
   <div class="details-wrapper">
-      <!-- <div class="loading" v-if="loadingContent">
+    <!-- <div class="loading" v-if="loadingContent">
 
         </div>-->
 
-       <Loading/>
-          <v-dialog
-    v-model="guestLoginPopup"
-      max-width="500px"
-      class="guest-dialog"
-    >
-           <v-card width="500px" class="mt-5 mx-a">
-          <v-card-title class="pb-0">
-            <h2 class="join-as-guest">Join as Guest</h2>
-             </v-card-title>
-            <v-card-text>
-              <v-form @submit.prevent="joinEventAsGuest">
-                <v-text-field
-                  id="userName"
-                  v-model="name"
-                  label="Username"
-                  prepend-icon="mdi-account-circle"
-                />
-              
-            
-             <v-card-actions class="guest-actions">
-                  <v-btn @click="joinedSuccess"  class="join-btn guest-btn" type="submit">JOIN</v-btn >
+    <Loading />
+    <v-dialog v-model="guestLoginPopup" max-width="500px" class="guest-dialog">
+      <v-card width="500px" class="mt-5 mx-a">
+        <v-card-title class="pb-0">
+          <h2 class="join-as-guest">Join as Guest</h2>
+        </v-card-title>
+        <v-card-text>
+          <v-form @submit.prevent="joinEventAsGuest">
+            <v-text-field
+              id="userName"
+              v-model="name"
+              label="Username"
+              prepend-icon="mdi-account-circle"
+            />
 
-                </v-card-actions>
-                  <transition name="fade" mode="out-in">
-                   <div v-if="isJoined" class="alert" role="alert">
-      <v-card
-        color="primary"
-        dark
-      >
-       <v-card-text class="succefully-joined">
-          Joined successfully!
-        </v-card-text>
-         </v-card>
-    </div>
-     </transition>
+            <v-card-actions class="guest-actions">
+              <v-btn
+                @click="joinedSuccess"
+                class="join-btn guest-btn"
+                type="submit"
+                >JOIN</v-btn
+              >
+            </v-card-actions>
+            <transition name="fade" mode="out-in">
+              <div v-if="isJoined" class="alert" role="alert">
+                <v-card color="primary" dark>
+                  <v-card-text class="succefully-joined">
+                    Joined successfully!
+                  </v-card-text>
+                </v-card>
+              </div>
+            </transition>
             <v-divider></v-divider>
-         
 
-          <v-card-title class="pb-0">
-            <h2 class="join-as-guest">OR</h2>
+            <v-card-title class="pb-0">
+              <h2 class="join-as-guest">OR</h2>
             </v-card-title>
-            
-          
 
-          <v-card-actions class="guest-actions">
-            <v-btn
-              class="login guest-btn"
-              type="submit"
-              :color="type === 'guest' ? 'secondary' : '#9933FF'"
-              >Login</v-btn
-            >
-            
-           
-          </v-card-actions>
-              </v-form>
-          </v-card-text>
-        </v-card>
+            <v-card-actions class="guest-actions">
+              <v-btn
+                class="login guest-btn"
+                type="submit"
+                :color="type === 'guest' ? 'secondary' : '#9933FF'"
+                >Login</v-btn
+              >
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-dialog>
-       
- 
+
     <vue-fontawesome icon="file" size="2" color="lightgray"></vue-fontawesome>
     <v-card :loading="loading" class="mx-auto my-12" outlined shaped tile>
       <div class="event-name">
-       
         <p>{{ currEvent.name }}</p>
-       
+
         <v-menu offset-y transition="slide-y-transition" bottom>
           <template v-slot:activator="{ on, attrs }">
             <img
@@ -94,10 +83,8 @@
                   class="dropdown-item"
                   >Delete</v-list-item-title
                 >
-
               </template>
-              
-           
+
               <v-card>
                 <v-card-title class="headline grey lighten-2">
                   Are you sure that you want to delete this event?
@@ -110,34 +97,26 @@
                   <v-btn color="primary" text @click="deleteEvent">
                     Yes
                   </v-btn>
-                  <v-btn color="primary" text @click="dialog = false">
+                  <v-btn color="primary" text @click.prevent="dialog = false">
                     Cancel
                   </v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
 
+            <v-dialog v-model="dialog2" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-list-item-title
+                  class="dropdown-item"
+                  v-bind="attrs"
+                  @click="openGuests = true"
+                  v-on="on"
+                  >See all guests
+                </v-list-item-title>
+              </template>
 
-<v-dialog
-      v-model="dialog2"
-      width="500"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        
-
-                <v-list-item-title class="dropdown-item"  v-bind="attrs" @click="openGuests=true"
-          v-on="on"
-              >See all guests
-            </v-list-item-title>
-
-                 </template>
-
-    <JoinedUsersList/>
-</v-dialog>
-
-
-
-
+              <JoinedUsersList />
+            </v-dialog>
           </v-list>
         </v-menu>
 
@@ -225,7 +204,12 @@
             <li>
               <div class="people-count">
                 <p>People going:</p>
-                <p>{{$store.state.specEvent.joinedUsers.length +$store.state.specEvent.joinedGuests.length }}</p>
+                <p>
+                  {{
+                    $store.state.specEvent.joinedUsers.length +
+                      $store.state.specEvent.joinedGuests.length
+                  }}
+                </p>
               </div>
             </li>
           </ul>
@@ -239,80 +223,86 @@
           <p>{{ currEvent.description }}</p>
         </div>
       </div>
-      <div class="btnJoin" >
-        
-       
-         <v-btn class="joinBut" elevation="11" x-large @click="joinEvent"> Join 
-         </v-btn>
-
-   
+      <div class="btnJoin">
+        <v-btn class="joinBut" elevation="11" x-large @click="joinEvent">
+          Join
+        </v-btn>
       </div>
-
-
-      
-      
     </v-card>
-    
   </div>
 </template>
 
 <script>
-import Loading from '../components/Loading.vue'
-import JoinedUsersList from '../components/JoinedUsersList.vue'
+import Loading from "../components/Loading.vue";
+import JoinedUsersList from "../components/JoinedUsersList.vue";
 export default {
-  components:{
-Loading,
-JoinedUsersList
+  components: {
+    Loading,
+    JoinedUsersList,
   },
   data() {
     return {
       dialog: false,
       deletePopup: false,
       afterDelete: false,
-      openGuests:false,
-      guestLoginPopup:false,
+      openGuests: false,
+      guestLoginPopup: false,
       isJoined: false,
       afterJoin: false,
-      name: 'Anonymous',
-      dialog2:false
-         
+      name: "Anonymous",
+      dialog2: false,
     };
   },
 
   watch: {
-    dialog(val) {
+    afterDelete(val) {
       if (!val) return;
 
       setTimeout(() => this.redirect(), 3000);
     },
 
     isJoined(val) {
-      if(!val) return; 
-        setTimeout(() => this.afterJoinRedirect(), 2000)
-      
-       
-
-    }
-
+      if (!val) return;
+      setTimeout(() => this.afterJoinRedirect(), 2000);
+    },
   },
 
   methods: {
     joinedSuccess() {
       this.isJoined = true;
     },
-    
-    getDate(eventDate) {
-      const monthNames = ["january", "february", "march", "april", "may", "june",
-  "july", "august", "september", "cotober", "november", "december"
-];
 
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    getDate(eventDate) {
+      const monthNames = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "cotober",
+        "november",
+        "december",
+      ];
+
+      var days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
 
       var startDt = new Date(eventDate);
       const startDay = startDt.getDate(),
         startMonth = monthNames[startDt.getMonth()],
-        day=days[startDt.getDay()],
-        date = day+" "+startDay + " " + (startMonth);
+        day = days[startDt.getDay()],
+        date = day + " " + startDay + " " + startMonth;
       return date;
     },
 
@@ -322,7 +312,6 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
     },
 
     afterJoinRedirect() {
-      
       this.$router.push("/");
     },
 
@@ -334,19 +323,19 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
       }
     },
 
-    async joinEventAsGuest(){
-     await this.$store.dispatch("guestJoinEvent", this.name)
+    async joinEventAsGuest() {
+      await this.$store.dispatch("guestJoinEvent", this.name);
     },
 
     editEvent() {
       this.$router.push("/update-event/" + this.currEvent.id);
     },
 
-    joinEvent(){
+    joinEvent() {
       console.log(this.$store.state.currLoggedInUser.id);
-       if(this.$store.state.currLoggedInUser.id===undefined){
-              this.guestLoginPopup=true
-       }
+      if (this.$store.state.currLoggedInUser.id === undefined) {
+        this.guestLoginPopup = true;
+      }
     },
 
     getTime(eventDate) {
@@ -364,10 +353,10 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
         let time = "00 : 00";
         return time;
       } else {
-        let minsLength=minutes+""
-        if(minsLength.length===1){
-           let time = hours + " : 0" + minutes;
-        return time;
+        let minsLength = minutes + "";
+        if (minsLength.length === 1) {
+          let time = hours + " : 0" + minutes;
+          return time;
         }
         let time = hours + " : " + minutes;
         return time;
@@ -376,8 +365,6 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
   },
 
   computed: {
-
-    
     currEvent() {
       return this.$store.state.specEvent;
     },
@@ -389,15 +376,19 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
     },
     ifHost() {
       let userIsHost =
-       this.hostUser.id === this.$store.state.currLoggedInUser.id
+        this.hostUser.id === this.$store.state.currLoggedInUser.id
           ? true
-          : false
+          : false;
       return userIsHost;
     },
 
-   getJoined(){
-    let joined=this.$store.state.specEvent.joinedUsers.includes(this.$store.state.currLoggedInUser)? true:false
-       return joined
+    getJoined() {
+      let joined = this.$store.state.specEvent.joinedUsers.includes(
+        this.$store.state.currLoggedInUser
+      )
+        ? true
+        : false;
+      return joined;
     },
 
     dates() {
@@ -423,9 +414,8 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
     },
   },
   async created() {
-    console.log('before fech', this.$store.state.loading);
+    console.log("before fech", this.$store.state.loading);
     await this.$store.dispatch("fetchSpecEvent", this.id);
-    
   },
 };
 </script>
@@ -445,12 +435,9 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
   justify-content: center;
 }
 
-
-
-.guest-actions{
+.guest-actions {
   justify-content: center !important;
 }
-
 
 .v-application .primary {
   background-color: var(--buttonPurpleSecondary) !important;
@@ -508,7 +495,7 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
   padding-top: 1vw;
 }
 
-.pb-0{
+.pb-0 {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -525,8 +512,8 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
   width: 100%;
 }
 
-.guest-btn{
-  width:300px  !important;
+.guest-btn {
+  width: 300px !important;
 }
 
 .detailedImg {
@@ -545,7 +532,8 @@ ul > li {
   text-align: center;
 }
 
-li > p, .people-count {
+li > p,
+.people-count {
   margin-top: 1.2vw;
   padding-left: 1vw;
   font-size: 1.2vw;
@@ -563,15 +551,14 @@ li > p, .people-count {
 }
 
 .joinBut {
-
- margin-bottom: 2vw;
+  margin-bottom: 2vw;
   margin-right: 5vw;
 }
 .btn-join {
   margin-top: -3vw;
   padding-bottom: 1vw;
   margin-right: 8vw;
-  min-height:50px;
+  min-height: 50px;
 }
 .theme--light.v-btn.v-btn--has-bg {
   background-color: var(--buttonPurple);
@@ -592,147 +579,140 @@ li > p, .people-count {
 }
 
 @media only screen and (max-width: 930px) {
- .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8]{
-   font-size:1.5vw;
- }
+  .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8] {
+    font-size: 1.5vw;
+  }
 
- .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8]{
-   width:12vw;
-   height:4vw;
- }
+  .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8] {
+    width: 12vw;
+    height: 4vw;
+  }
 
- .event-desc, .event-info{
-   height:70vh;
- }
+  .event-desc,
+  .event-info {
+    height: 70vh;
+  }
 
- li > p, .event-desc>p, .people-count{
-   font-size:1.5vw;
- }
-
-
+  li > p,
+  .event-desc > p,
+  .people-count {
+    font-size: 1.5vw;
+  }
 }
 
 @media only screen and (max-width: 790px) {
-  
-.detailedImg {
-  width: 12%;
-}
+  .detailedImg {
+    width: 12%;
+  }
 
-.settings{
-  width:5%;
-}
+  .settings {
+    width: 5%;
+  }
 
-ul>li{
-  margin-top:4vw;
-}
+  ul > li {
+    margin-top: 4vw;
+  }
 }
 
 @media only screen and (max-width: 610px) {
+  li > p,
+  .event-desc > p,
+  .people-count {
+    font-size: 2vh;
+  }
 
-  
- li > p, .event-desc>p, .people-count{
-   font-size:2vh;
- }
+  .event-details {
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: 1fr;
+  }
 
+  .event-info {
+    display: flex;
+    flex-direction: column;
+  }
+  .detailedImg {
+    width: 40px;
+  }
 
-.event-details{
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: 1fr;
-}
+  .event-desc {
+    margin-left: 2vw;
+  }
+  .settings {
+    width: 5%;
+  }
 
-.event-info{
-  display: flex;
-  flex-direction: column;
-}
- .detailedImg {
-  width: 40px;
-}
+  ul > li {
+    margin-top: 5vw;
+  }
 
-.event-desc{
- 
-  margin-left:2vw;
-}
-.settings{
-  width:5%;
-}
-
-ul>li{
-  margin-top:5vw;
-}
-
-.event-name>p{
-  font-weight: 500;
-  font-size:1.8em;
-}
-
+  .event-name > p {
+    font-weight: 500;
+    font-size: 1.8em;
+  }
 }
 
 @media only screen and (max-width: 510px) {
+  .event-desc {
+    margin-top: -2vw;
+  }
 
-.event-desc{
- 
-  margin-top:-2vw;
-}
+  .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8] {
+    font-size: 2vw;
+  }
 
-.v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8]{
-   font-size:2vw;
- }
+  .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8] {
+    width: 20vw;
+    height: 5vw;
+  }
 
- .v-btn:not(.v-btn--round).v-size--x-large[data-v-14d957f8]{
-   width:20vw;
-   height:5vw;
- }
-
- .event-info{
-  height:auto;
-  margin-bottom:5vh;
- }
-
+  .event-info {
+    height: auto;
+    margin-bottom: 5vh;
+  }
 }
 
 @media only screen and (max-width: 510px) {
   .detailedImg {
-  width: 30px;
+    width: 30px;
+  }
+  .settings {
+    width: 5%;
+  }
+
+  li > p,
+  .event-desc > p,
+  .people-count {
+    font-size: 1.4vh;
+  }
 }
-.settings{
-  width:5%;
+@media only screen and (max-width: 360px) {
+  li > p,
+  .event-desc > p,
+  .people-count {
+    font-size: 1.2vh;
+  }
+
+  .detailedImg {
+    width: 20px;
+  }
+  .settings {
+    width: 5%;
+  }
+
+  .event-info {
+    margin-top: -15vh;
+  }
+
+  .event-desc {
+    margin-top: -10vh;
+  }
+  .event-name {
+    font-size: 2.5vw;
+  }
+}
+@media only screen and (min-width: 340px) {
 }
 
- li > p, .event-desc>p, .people-count{
-   font-size:1.4vh;
- }
-}
-@media only screen and (max-width: 360px){
- li > p, .event-desc>p, .people-count{
-   font-size:1.2vh;
- }
-
-   .detailedImg {
-  width: 20px;
-}
-.settings{
-  width:5%;
-}
-
-
-
- .event-info{
-
-  margin-top:-15vh;
- }
-
-
-.event-desc{
-  margin-top:-10vh;
-}
-.event-name{
-  font-size:2.5vw;
-}
-}
-@media only screen and (min-width: 340px){
- 
-}
- 
 .alert {
   background-color: rgba(231, 223, 240, 0.797);
   padding: 3px;
@@ -751,7 +731,6 @@ ul>li{
 .succefully-joined {
   display: flex;
   justify-content: center;
-  align-items: center;  
+  align-items: center;
 }
-
 </style>
